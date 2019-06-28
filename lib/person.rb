66fb4d3e9bcfb2f5f26dd3eda2ac8f786d778165
Silvia Mendoza <silviamendoza223@yahoo.com>
2019-06-28 03:27:05 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
 class Person
-  #your code here
+  attr_accessor (:name, :birthday,:hair_color, 
+  :eye_color, :height, :weight, :handed, :weight, :handed,
+  :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width)
 end
+def bob_attributes = {name: "Bob", hair_color: "Brown"}
+  @name => "Bob"
+  @hair_color => "Brown"
+end
+
+
+
+
+
+
+`end
